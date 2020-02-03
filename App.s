4bcffeb5a9ec12a2ba9ae5b9c6f6b6ca6ba5c7f6
Lexes Mantiquilla <mystery32120@gmail.com>
2020-02-03 15:01:04 +0000

    Add temporary itoa

--- App.s
+++ App.s
@@ -1,3 +1,6 @@
+  AREA  memory, DATA, READWRITE
+num space 11
+
   AREA  AsmTemplate, CODE, READONLY
   IMPORT  main
 
@@ -10,6 +13,7 @@ start
 IO1DIR  EQU  0xE0028018
 IO1SET  EQU  0xE0028014
 IO1CLR  EQU  0xE002801C
+LEN     EQU  9
 
   ; initialise SP
   ldr r13, =0x40002000 ; initialise SP to top of stack
@@ -19,7 +23,7 @@ IO1CLR  EQU  0xE002801C
   ;
   bl initLed
   ldr r4, =0x40000000
-  ldr r0, =0
+  ldr r0, =-1049
   mov r1, r4
   bl itoa
   ldr r7, =0x000f0000         ; led_mask = 0x000f0000
@@ -32,12 +36,12 @@ forchr
   beq eforchr                 ;   {
   mov r0, r6
   bl displayNum               ;     displayNum(*str)
-  bl delay                    ;     delay()
+  ; bl delay                    ;     delay()
   str r7, [r8]                ;     turn_off_leds(P1.19-P1.16)
   ldrb r6, [r5, #1]!
   b forchr                    ;   }
 eforchr
-  bl delay                    ;   delay()
+  ; bl delay                    ;   delay()
   b whileT                    ; }
 
 stop b stop
@@ -106,6 +110,56 @@ reWhileN
   mov r0, r4                  ; reversed = result
   ldmfd sp!, {r4, pc}
 
+;;
+;; itoa
+;; converts a signed 32 bit integer to ascii
+;; parameters:
+;;   r0 - num which is a 32 bit integer
+;;   r1 - address containing sufficient space to store ascii version of the
+;;        integer
+;; return:
+;;   r0 - the size of the string
+;;
+;itoa
+;  stmfd sp!, {r4-r6, lr}
+;  mov r3, r0                    ; r3 = num
+;  mov r4, r1                    ; r4 = address
+;  mov r5, #0                    ; i = 0
+;  cmp r3, #0                    ; if (num < 0)
+;  bge itoaeIf                   ; {
+;  mov r2, #'-'                  ;   neg_char = '-'
+;  strb r2, [r4, r5]             ;   result[i] = neg_char
+;  mvn r3, r3                    ;   num ~= num
+;  add r3, r3, #1                ;   num += 1
+;  add r5, r5, #1                ;   i++
+;itoaeIf                         ; }
+;  mov r6, #0                    ; j = 0
+;itoaWhileN                      ; do {
+;  mov r0, r3                    ;   numerator = num
+;  mov r1, #10                   ;   denominator = 10
+;  bl udiv                       ;   div(num, 10)
+;  str r1, [sp, #-4]!            ;   push(num % 10)
+;  mov r3, r0                    ;   num /= 10
+;  add r6, r6, #1                ;   j++
+;  cmp r3, #0                    ;
+;  beq itoaeWhile                ; } while (num != 0)
+;  b itoaWhileN
+;itoaeWhile
+;itoaWhilej
+;  cmp r6, #0                    ; while (j > 0)
+;  ble itoaeWhilej               ; {
+;  ldr r0, [sp], #4              ;   digit = pop()
+;  add r0, r0, #'0'              ;   digit = Character.valueOf(digit)
+;  strb r0, [r4, r5]             ;   result[i] = digit
+;  add r5, r5, #1                ;   i++
+;  sub r6, r6, #1                ;   j--
+;  b itoaWhilej                  ; }
+;itoaeWhilej
+;  mov r1, #'\0'                 ; null = '\0'
+;  strb r1, [r4, r5]             ; result[i] = '\0'
+;  mov r0, r5                    ; size = i
+;  ldmfd sp!, {r4-r6, pc}
+
 ;
 ; itoa
 ; converts a signed 32 bit integer to ascii
@@ -127,34 +181,45 @@ itoa
   strb r2, [r4, r5]             ;   result[i] = neg_char
   mvn r3, r3                    ;   num ~= num
   add r3, r3, #1                ;   num += 1
+  mvn r0, r0                    ;   orig_num ~= orig_num
+  add r0, r0, #1                ;   orig_num += 1
   add r5, r5, #1                ;   i++
 itoaeIf                         ; }
-  mov r6, #0                    ; j = 0
-itoaWhileN                      ; do {
-  mov r0, r3                    ;   numerator = num
-  mov r1, #10                   ;   denominator = 10
-  bl udiv                       ;   div(num, 10)
-  str r1, [sp, #-4]!            ;   push(num % 10)
-  mov r3, r0                    ;   num /= 10
-  add r6, r6, #1                ;   j++
-  cmp r3, #0                    ;
-  beq itoaeWhile                ; } while (num != 0)
-  b itoaWhileN
-itoaeWhile
-itoaWhilej
-  cmp r6, #0                    ; while (j > 0)
-  ble itoaeWhilej               ; {
-  ldr r0, [sp], #4              ;   digit = pop()
-  add r0, r0, #'0'              ;   digit = Character.valueOf(digit)
-  strb r0, [r4, r5]             ;   result[i] = digit
-  add r5, r5, #1                ;   i++
-  sub r6, r6, #1                ;   j--
-  b itoaWhilej                  ; }
-itoaeWhilej
+  ldr r6, =table                ; table[]
+  mov r7, #LEN                  ;
+  sub r7, r7, #1                ;
+itoaforj
+  cmp r7, #0                    ; for (j = LEN - 1; j >= 0; j--)
+  blt itoaeforj                 ; {
+  ldr r8, [r6, r7, lsl #2]      ;   divisor = table[j]
+  mov r9, #0                    ;   counter = 0
+  sub r3, r3, r8                ;   num -= divisor
+itoaWhileN
+  cmp r3, #0                    ;   while (num > 0)
+  ble itoaeWhileN               ;   {
+  sub r3, r3, r8                ;     num -= divisor
+  add r9, r9, #1                ;     counter++
+  b itoaWhileN                  ;   }
+itoaeWhileN
+  add r3, r3, r8                ;   num += divisor
+  cmp r3, r0                    ;   if (num != orig_num)
+  beq itoaeif0                  ;   {
+  add r9, r9, #'0'              ;     digit = Character.valueOf(digit)
+  strb r9, [r4, r5]             ;     result[i] = digit
+  add r5, r5, #1                ;     i++
+itoaeif0                        ;   }
+  sub r7, r7, #1
+  b itoaforj                    ; }
+itoaeforj
+  add r3, r3, #'0'              ; digit = Character.valueOf(digit)
+  strb r3, [r4, r5]             ; result[i] = digit
+  add r5, r5, #1                ; i++
   mov r1, #'\0'                 ; null = '\0'
   strb r1, [r4, r5]             ; result[i] = '\0'
   mov r0, r5                    ; size = i
   ldmfd sp!, {r4-r6, pc}
+
+
 ;
 ; initLed
 ; initialise P1.19-P1.16 to output and turn them off
@@ -235,5 +300,16 @@ UDIV1  SUB  R6, R6, #1      ; i = i - 1 (s1)
 UDIV2  MOV  R0, R2        ; R0 = quotient
   MOV  R1, R3        ; R1 = remaider
   POP {R4-R6, PC}  ; pop R2-R6 and return
+
+table
+  dcd 10
+  dcd 100
+  dcd 1000
+  dcd 10000
+  dcd 100000
+  dcd 1000000
+  dcd 10000000
+  dcd 100000000
+  dcd 1000000000
 
   END
